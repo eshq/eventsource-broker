4876ce326020bfc98fca3ca51f962cdd8dca261e
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2013-04-23 17:25:13 -0700

    Improve index use when fetching event buffer

--- src/Models/Event.hs
+++ src/Models/Event.hs
@@ -36,7 +36,7 @@ store db event = case eventId event of
 
 since :: DB -> Text -> Text -> Text -> IO (Either Failure [Event])
 since db user channel eid = do
-    result <- run db $ rest =<< find ((select ["u" =: user, "c" =: channel, "_id" =: ["$gt" =: oid]] "events") {limit = 50, sort = ["$natural" := Float 1]})
+    result <- run db $ rest =<< find ((select ["u" =: user, "c" =: channel, "_id" =: ["$gt" =: oid]] "events") {limit = 50, sort = ["_id" := Float 1]})
     return $ fmap (map  constructor) result
   where
     oid :: ObjectId
